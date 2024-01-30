c54c1f5078b0f8b45c55fc6104b901b2b8f9df89
Tim Volkov <volkov.timm@gmail.com>
2024-01-30 14:20:51 +0500

    using dotenv

--- hexlet_django_blog/article/models.py
+++ hexlet_django_blog/article/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    name = models.CharField(max_length=200) # название статьи
+    body = models.TextField() # тело статьи
+    timestamp = models.DateTimeField(auto_now_add=True)
--- hexlet_django_blog/settings.py
+++ hexlet_django_blog/settings.py
@@ -11,6 +11,13 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+SECRET_KEY = os.getenv('SECRET_KEY')
+DEBUG = os.getenv('DEBUG', False)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
